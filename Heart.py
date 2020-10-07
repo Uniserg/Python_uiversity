a3f9be3c577c830c6697ec884126e1c73cd55539
Serg <serg10101_2001@mail.ru>
2020-10-07 14:11:16 +0300

    Heart add

--- Heart.py
+++ Heart.py
@@ -18,7 +18,6 @@ polar_rose = lambda x, y: sin(6 * y) * x
 astroid = lambda x, y: (cos(y) ** 20 + sin(y) ** 20) ** (1 / 2) * x
 jmap = lambda x, y: (2 - 0.5 * sin(50 * y) + cos(7 * y)) * x
 
-
 figures = {"Сердце": heart, 'Полярная роза': polar_rose, 'Астроида': astroid, 'Снежинка': jmap}
 
 
@@ -32,9 +31,8 @@ def motion(func, size, width_line=0):
     fr = resolutionX / 2, resolutionY / 2
     while angle < start + 2 * pi + step:
         to = from_polar_system(func, size, angle, resolutionX / 2, resolutionY / 2)
-        fr_s = fr[0] + step * cos(angle), fr[1] + step * sin(angle)
         c.create_polygon(resolutionX / 2, resolutionY / 2, fr, to, fill='red', outline='red')
-        c.create_polygon(fr_s, fr, to, fill='white', outline='white', width=width_line)
+        c.create_polygon(fr, to, outline='white', width=width_line)
         fr = to
         angle += step
         c.update()
